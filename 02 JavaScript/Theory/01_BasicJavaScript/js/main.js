345dc8b4cc7e31425035d1201d452c2732dad581
JGG <javieron.garcia@gmail.com>
2020-12-19 00:44:58 +0100

    S02_01

---
+++ 02 JavaScript/Theory/01_BasicJavaScript/js/main.js
@@ -0,0 +1,123 @@
+// 1
+
+// single line comment
+/* multiple line comment
+ */
+
+//2
+
+var myName;
+
+//  3
+
+// Setup
+var a;
+
+// Only change code below this line
+
+a = 7;
+
+// 4
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+b = a;
+
+// 5
+var myVar = 9;
+
+// 6
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5
+b = 10
+c = 'I am a'
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 7
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 8
+var sum = 10 + 10;
+
+// 9
+var difference = 45 - 33;
+
+// 10
+var product = 8 * 10;
+
+// 11
+var quotient = 66 / 33;
+
+// 12
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+// 13
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+// 15
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 1.1;
+
+// 16
+var product = 2.0 * 2.5;
+
+// 17
+var quotient = 4.4 / 2.0; // Change this line
+
+// 18
+var remainder = 11 % 3;
+
+// 19
+// Only change code below this line
+a += 12;
+b += b;
+c += 7;
+
+// 20
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 21
+a *= 5;
+b *= 3;
+c *= 10;
+
+// 22
+a /= 12;
+b /= 4;
+c /= 11;
+
+// 23
+var myFirstName = 'Javier';
+var myLastName = 'García Gómez';
+
+// 24
+var myStr = "I am a \"double quoted\" string inside \"double quotes\""; // Change this line
